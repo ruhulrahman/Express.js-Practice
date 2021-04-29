30ceafde357ad91f5b98590e2b6983eb14991cf0
ruhulrahman <ruhulrahman2233@gmail.com>
2021-04-29 20:25:02 +0600

    routing added

--- practice-1/index.js
+++ practice-1/index.js
@@ -3,7 +3,19 @@ var app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Home Page!')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About Page!')
+})
+
+app.get('/contact', (req, res) => {
+    res.send('Contact Page!')
+})
+
+app.get('/mission', (req, res) => {
+    res.send('Mission Page!')
 })
 
 
